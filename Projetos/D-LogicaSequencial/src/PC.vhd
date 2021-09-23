75a4ba7cb2e5066360cbebe550edb2c81e717d83
MineManiac <matheusbarrosr@gmail.com>
2021-09-23 20:49:05 -0300

    PC conceito B no entanto nao esta passando nos testes devido aos componentes, rtl feito tambem

--- Projetos/D-LogicaSequencial/src/PC.vhd
+++ Projetos/D-LogicaSequencial/src/PC.vhd
@@ -27,24 +27,68 @@ end entity;
 architecture arch of PC is
 
  signal muxOut : std_logic_vector(15 downto 0);
-
-  component Inc16 is
-      port(
-          a   :  in STD_LOGIC_VECTOR(15 downto 0);
-          q   : out STD_LOGIC_VECTOR(15 downto 0)
-          );
+ component Inc16 is
+    port(
+        a:  in STD_LOGIC_VECTOR(15 downto 0);
+        q: out STD_LOGIC_VECTOR(15 downto 0)
+        );
   end component;
 
   component Register16 is
+    port(
+        clock:   in STD_LOGIC;
+        input:   in STD_LOGIC_VECTOR(15 downto 0);
+        load:    in STD_LOGIC;
+        output: out STD_LOGIC_VECTOR(15 downto 0)
+      );
+  end component;
+  
+  component Mux16 is
       port(
-          clock:   in STD_LOGIC;
-          input:   in STD_LOGIC_VECTOR(15 downto 0);
-          load:    in STD_LOGIC;
-          output: out STD_LOGIC_VECTOR(15 downto 0)
-        );
-    end component;
+          a: in STD_LOGIC_VECTOR(15 downto 0);
+          b: in STD_LOGIC_VECTOR(15 downto 0);
+          sel: in  STD_LOGIC;
+          q:   out STD_LOGIC_VECTOR(15 downto 0));
+  end component;
+  
+signal outInc, outMuxInc, outMuxLoad, outMuxReset,outRegister: STD_LOGIC_VECTOR(15 downto 0);
 
 begin
 
+    INC: Inc16 port map (         
+        a => outRegister,             
+        q => outInc          
+    );
+	 
+	 
+    REG: Register16 port map (     
+        clock => clock,
+        input => outMuxReset,
+        load  => '1',                       
+        output => outRegister
+    );
+
+    MuxInc: Mux16 port map (    
+        a => outRegister,             
+        b => outInc,         
+        sel => increment,     
+        q => outMuxInc
+    );
+
+    MuxLoad: Mux16 port map(
+        a => outMuxInc,                
+        b => input,              
+        sel => load,
+        q => outMuxLoad 
+    );
+
+    MuxReset: Mux16 port map(
+        a => outMuxLoad,          
+        b =>  "0000000000000000",   
+        sel => reset,
+        q => outMuxReset 
+    );    
+
+    output <= outRegister;                         
 
 end architecture;
