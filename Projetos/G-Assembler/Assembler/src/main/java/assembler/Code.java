a191cc25535575f42934e4ca712cee61b16f68a5
renatex333 <renato9302@hotmail.com>
2021-11-10 15:18:45 -0300

    Metodo comp de Code.java feito e passando nos testes

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -45,8 +45,145 @@ public class Code {
      * @return Opcode (String de 7 bits) com código em linguagem de máquina para a instrução.
      */
     public static String comp(String[] mnemnonic) {
-        /* TODO: implementar */
-    	return "";
+        String instrucao;
+        switch (mnemnonic[0]){
+            case "movw":
+                switch (mnemnonic[1]) {
+                    case "%A":
+                        return "000110000";
+                    case "%D":
+                        return "000001100";
+                    case "(%A)":
+                        return "001110000";
+                }
+            case "addw":
+                instrucao = mnemnonic[1] + " " + mnemnonic[2];
+                switch (instrucao) {
+                    case "%A %D":
+                    case "%D %A":
+                        return "000000010";
+                    case "(%A) %D":
+                    case "%D (%A)":
+                        return "001000010";
+                    case "$1 %D":
+                    case "%D $1":
+                        return "000011111";
+                    case "$1 %A":
+                    case "%A $1":
+                        return "000110111";
+                    case "$1 (%A)":
+                    case "(%A) $1":
+                        return "001110111";
+
+                }
+            case "incw":
+                instrucao = mnemnonic[1];
+                switch (instrucao) {
+                    case "%A":
+                        return "000110111";
+                    case "(%A)":
+                        return "001110111";
+                    case "%D":
+                        return "000011111";
+                }
+            case "subw":
+                instrucao = mnemnonic[1] + " " + mnemnonic[2];
+                switch (instrucao) {
+                    case "%A %D":
+                        return "000000111";
+                    case "%D %A":
+                        return "000010011";
+                    case "(%A) %D":
+                        return "001000111";
+                    case "%D (%A)":
+                        return "001010011";
+                    case "%D $1":
+                        return "000001110";
+                    case "%A $1":
+                        return "000110010";
+                    case "(%A) $1":
+                        return "001110010";
+                }
+            case "rsubw":
+                instrucao = mnemnonic[2] + " " + mnemnonic[1];
+                switch (instrucao) {
+                    case "%A %D":
+                        return "000000111";
+                    case "%D %A":
+                        return "000010011";
+                    case "(%A) %D":
+                        return "001000111";
+                    case "%D (%A)":
+                        return "001010011";
+                    case "%D $1":
+                        return "000001110";
+                    case "%A $1":
+                        return "000110010";
+                    case "(%A) $1":
+                        return "001110010";
+                }
+            case "decw":
+                instrucao = mnemnonic[1];
+                switch (instrucao) {
+                    case "%A":
+                        return "000110010";
+                    case "(%A)":
+                        return "001110010";
+                    case "%D":
+                        return "000001110";
+
+                }
+            case "notw":
+                instrucao = mnemnonic[1];
+                switch (instrucao) {
+                    case "%A":
+                        return "000110001";
+                    case "(%A)":
+                        return "001110001";
+                    case "%D":
+                        return "000001101";
+                }
+            case "negw":
+                instrucao = mnemnonic[1];
+                switch (instrucao) {
+                    case "%A":
+                        return "000110011";
+                    case "(%A)":
+                        return "001110011";
+                    case "%D":
+                        return "000001111";
+                }
+            case "andw":
+                instrucao = mnemnonic[1] + " " + mnemnonic[2];
+                switch (instrucao) {
+                    case "%A %D":
+                    case "%D %A":
+                        return "000000000";
+                    case "(%A) %D":
+                    case "%D (%A)":
+                        return "001000000";
+                }
+            case "orw":
+                instrucao = mnemnonic[1] + " " + mnemnonic[2];
+                switch (instrucao) {
+                    case "%A %D":
+                    case "%D %A":
+                        return "000010101";
+                    case "(%A) %D":
+                    case "%D (%A)":
+                        return "001010101";
+                }
+            case "jmp":
+            case "jg":
+            case "je":
+            case "jge":
+            case "jl":
+            case "jne":
+            case "jle":
+                return "000001100";
+
+            default: return "000000000";
+        }
     }
 
     /**
